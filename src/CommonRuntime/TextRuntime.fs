cd2658bc18a2fa02f05f65871b9215b4b47eee91
Daniel Bradley <daniel@danielbradley.net>
2014-09-25 22:41:18 +0100

    Use ISO 8601 with invariant culture to output dates

--- src/CommonRuntime/TextRuntime.fs
+++ src/CommonRuntime/TextRuntime.fs
@@ -88,7 +88,7 @@ type TextRuntime =
 
   static member ConvertDateTimeBack(cultureStr, value:DateTime option) = 
     match value with
-    | Some value -> value.ToString(TextRuntime.GetCulture cultureStr)
+    | Some value -> value.ToString("yyyy-MM-ddTHH:mm:ss.fffK", CultureInfo.InvariantCulture)
     | None -> ""
 
   static member ConvertGuidBack(value:Guid option) = 
